eb2a39522f93cae2d860753b44a71f907b1096bb
1336369198 <1336369198@qq.com>
2021-06-07 10:17:33 +0800

    to update

--- src/main.js
+++ src/main.js
@@ -2,8 +2,14 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+
+// 1.Element
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
+
+// 2.public.css
+import "./style/public.css"
+// 3.
 Vue.use(ElementUI);
 Vue.config.productionTip = false;
 
---
+++ vue.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    lintOnSave: false,
+    devServer: {
+        port: 8080, // 端口号
+        host: "192.168.0.140",
+        https: false, // https:{type:Boolean}
+        open: true, //配置自动启动浏览器
+    }
+}
